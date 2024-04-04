8824e21129c26827d92135cfc6fba6f3bbeccdf3
José Alan <104922063+Alanz0ka@users.noreply.github.com>
2024-04-04 15:35:13 -0300

    Create test-connection.js

---
+++ src/connection/test-connection.js
@@ -0,0 +1,9 @@
+const OpenConnection = require("./openconnection.js");
+
+const openConnection = new OpenConnection();
+
+openConnection.connect();
+
+setTimeout(()=>{
+    openConnection.end();
+}, 4000)
