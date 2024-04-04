37af0419e57bbd289103308abf1f350219d4a5b9
José Alan <104922063+Alanz0ka@users.noreply.github.com>
2024-04-04 16:05:14 -0300

    Create Titulos.js

---
+++ src/connection/Titulos.js
@@ -0,0 +1,10 @@
+class Titulos{
+    constructor(){
+        this.id = null,
+        this.tipo = "",
+        this.titulo = "",
+        this.diretor = "",
+        this.elenco = "",
+        this.genero = "",
+    }
+}
