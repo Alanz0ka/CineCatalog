49d478d2a908034f2b31a388b0df9b0fc34712bf
José Alan <104922063+Alanz0ka@users.noreply.github.com>
2024-04-05 15:22:09 -0300

    Criando o encapsulamento
    Criando os métodos encapsulados

--- src/connection/Titulos.js
+++ src/connection/Titulos.js
@@ -7,4 +7,46 @@ class Titulos{
         this.elenco = "",
         this.genero = ""
     }
+
+    getId(){
+        return this.id;
+    }
+    setId(novo){
+        this.id = novo;
+    }
+
+    getTipo(){
+        return this.tipo;
+    }
+    setTipo(novo){
+        this.tipo = novo;
+    }
+
+    getTitulo(){
+        return this.titulo;
+    }
+    setTitulo(novo){
+        this.titulo = novo;
+    }
+
+    getDiretor(){
+        return this.diretor;
+    }
+    setDiretor(novo){
+        this.diretor = novo
+    }
+
+    getElenco(){
+        return this.elenco;
+    }
+    setElenco(novo){
+        this.elenco = novo;
+    }
+
+    getGenero(){
+        return this.genero;
+    }
+    setGenero(novo){
+        this.genero = novo;
+    }
 }
